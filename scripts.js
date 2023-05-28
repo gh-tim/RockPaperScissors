636d7841da7aa013e1fe5a79cc3d81eee0aab350
timothy <timokwek@gmail.com>
2023-05-28 16:53:24 +0800

    Create function to evaluate choices

--- scripts.js
+++ scripts.js
@@ -1,10 +1,39 @@
 //initialize constants of rock paper scissors
-const choices = ["rock", "paper", "scissors"];
-
 //get computer's choice of rock, paper, or scissors
-function getComputerChoice([choices]) {
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
     let randomChoice = Math.floor(Math.random() * choices.length);
     return choices[randomChoice];
 }
 
-console.log(getComputerChoice([choices]));
+//declare computerSelection and playerSelection variables
+let computerSelection = getComputerChoice();
+console.log(computerSelection);
+
+let playerSelection = prompt("Make your selection (Rock, Paper, Scissors): ").toLowerCase();
+console.log(playerSelection);
+
+//function to evaluate player choice against computer choice
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `Tie! ${playerSelection} matches ${computerSelection}`;
+    }   
+    else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "You win! Rock beats Scissors";
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You win! Paper beats Rock";
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "You win! Scissors beats Paper";
+    }
+    else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
+
+//print round results
+console.log(playRound(playerSelection, computerSelection));
+
+
+
