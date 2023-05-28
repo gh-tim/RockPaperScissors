4a4871b8d452d27f3261b224f9c8751f80be475c
timothy <timokwek@gmail.com>
2023-05-28 17:57:21 +0800

    Fix game() 'for' loop to repeat for 5 rounds

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ function game() {
     let computerScore = 0;
 
     //for loop to repeat 5 rounds
-    for(let i = 0; i <= 5; i++) {
+    for (let i = 0; i <= 4; i++) {
         //initialize constants of rock paper scissors
         //get computer's choice of rock, paper, or scissors
         function getComputerChoice() {
@@ -16,34 +16,46 @@ function game() {
     
         //declare computerSelection and playerSelection variables
         let computerSelection = getComputerChoice();
-        console.log(computerSelection);
     
         let playerSelection = prompt("Make your selection (Rock, Paper, Scissors): ").toLowerCase();
         console.log(playerSelection);
 
         function playRound(playerSelection, computerSelection) {
             if (playerSelection === computerSelection) {
-                return `Tie! ${playerSelection} matches ${computerSelection}`;
+                console.log(`Tie! You chose ${playerSelection} which matches CPU's ${computerSelection}`);
             }   
             else if (playerSelection === "rock" && computerSelection === "scissors") {
                 playerScore++;
-                return "You win! Rock beats Scissors";
+                console.log(`You win! Your ${playerSelection} beats CPU's ${computerSelection}`);
             }
             else if (playerSelection === "paper" && computerSelection === "rock") {
                 playerScore++;
-                return "You win! Paper beats Rock";
+                console.log(`You win! Your ${playerSelection} beats CPU's ${computerSelection}`);
             }
             else if (playerSelection === "scissors" && computerSelection === "paper") {
                 playerScore++;
-                return "You win! Scissors beats Paper";
+                console.log(`You win! Your ${playerSelection} beats CPU's ${computerSelection}`);
             }
             else {
                 computerScore++;
-                return `You lose! ${computerSelection} beats ${playerSelection}`;
+                console.log(`You lose! CPU's ${computerSelection} beats your ${playerSelection}`);
             }
         }
         console.log(playRound(playerSelection, computerSelection));
-        return `Player: ${playerScore} | Computer: ${computerScore}`;
+        console.log(`Player: ${playerScore} | CPU: ${computerScore}`);
+
+    }
+    if (playerScore > computerScore) {
+        console.log("Player wins!");
+    }
+    else if (computerScore > playerScore) {
+        console.log("CPU wins!");
+    }
+    else if (playerScore === computerScore) {
+        console.log("It's a tie!");
+    }
+    else {
+        console.log("Error! Please refresh the page");
     }
 }
 
